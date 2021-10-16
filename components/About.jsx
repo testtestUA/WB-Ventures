49299bdf1ed56e131b7cbb15ccaa5740833c28c9
cenkwb <91887740+cenkwb@users.noreply.github.com>
2021-10-16 11:12:15 +0300

    noreferrer eklendi noopener kaldırıldı

--- components/About.jsx
+++ components/About.jsx
@@ -94,7 +94,7 @@ const About = () => {
       <Container>
         <Title>{about.subtitle}</Title>
         <LogoContainer>
-          <a target="_blank" rel="noopener noreferrer" href="https://www.winxbet.net"><Image alt='logo' src='/winxbet.png' width='164px' height='41px' /></a>
+          <a target="_blank" rel="noreferrer" href="https://www.winxbet.net"><Image alt='logo' src='/winxbet.png' width='164px' height='41px' /></a>
           <Image alt='logo' src='/betbase.svg' width='120px' height='34px'/>
         </LogoContainer>
       </Container>
